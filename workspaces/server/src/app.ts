36517861e6b3e36191cf145b4d62d761533959da
Damian Zamolski <damian.zamola.zamolski@gmail.com>
2022-05-19 16:29:55 +0200

    Add mongodb connection

--- workspaces/server/src/app.ts
+++ workspaces/server/src/app.ts
@@ -2,10 +2,14 @@ import Koa from 'koa';
 import cors from '@koa/cors';
 import body from 'koa-body';
 import Router from '@koa/router';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient('mongodb://127.0.0.1:27017');
+client.connect();
 
 const router = new Router();
-router.get('name', '/', async (ctx) => {
-  ctx.response.status = 200;
+router.get('name', '/', async (context) => {
+  context.body = await client.db('dz').collection('champions').find().toArray();
 });
 
 const middlewares = [cors(), body(), router.routes()];
